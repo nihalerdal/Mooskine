fc630c29af5f7505818fd3bc963e8ff7047170ac
Nihal Erdal <nihalerdall@gmail.com>
2021-07-29 20:36:13 -0700

    autosaving()

--- Mooskine-starter/Mooskine/Model/DataController.swift
+++ Mooskine-starter/Mooskine/Model/DataController.swift
@@ -28,8 +28,26 @@ class DataController{
             guard error == nil else {
                 fatalError(error!.localizedDescription)
             }
+            self.autoSaveViewContext(interval: 3)
             completion?()
         }
     }
-    
 }
+
+extension DataController {
+    func autoSaveViewContext(interval: TimeInterval = 30){
+        print("autosaving")
+        guard interval > 0 else {
+            print("cannot set negative autosave interval")
+            return
+        }
+        if viewContext.hasChanges{
+            try? viewContext.save()
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + interval) {
+            self.autoSaveViewContext(interval: interval)
+        }
+    }
+}
+
+
